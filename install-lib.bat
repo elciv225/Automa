45ecc182950acfbcdcb1754d925309641c4c5369
Eliel Assy <elielassy06@gmail.com>
2025-05-02 12:18:09 +0000

    Je passe sur la nouvelle machine HP

---
+++ install-lib.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Répertoire contenant les .jar
+set LIB_DIR=lib
+
+REM Vérifie si le dossier lib existe
+if not exist "%LIB_DIR%" (
+    echo Le dossier %LIB_DIR% n'existe pas.
+    exit /b
+)
+
+for %%f in (%LIB_DIR%\*.jar) do (
+    set FILE=%%f
+    set NAME=%%~nf
+    echo.
+    echo 📦 Installation de !FILE! ...
+    mvn install:install-file ^
+        -Dfile=!FILE! ^
+        -DgroupId=local ^
+        -DartifactId=!NAME! ^
+        -Dversion=1.0 ^
+        -Dpackaging=jar
+)
+
+echo.
+echo ✅ Tous les JAR du dossier %LIB_DIR% ont été installés dans le dépôt Maven local.
+pause
